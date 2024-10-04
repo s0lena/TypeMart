93bb1dd76fbe3ef6280611745e367dc7048551c1
s0lena <solena.work@gmail.com>
2024-10-04 12:41:20 +0300

    Finished task

--- index.ts
+++ index.ts
@@ -19,3 +19,19 @@ if (Number(product.price) >= 25) {
 } else {
   shipping = 5;
 }
+
+if (shippingAddress.includes("New York")) {
+  taxPercent = 0.1;
+} else {
+  taxPercent = 0.05;
+}
+
+taxTotal = Number(product.price) * taxPercent;
+total = Number(product.price) + taxTotal;
+
+console.log(`Product:  ${product.name}
+Address:  ${shippingAddress}
+Price:    $${product.price}
+Tax:      $${taxTotal.toFixed(2)}
+Shipping: $${shipping.toFixed(2)}
+Total:    $${total.toFixed(2)}`);
